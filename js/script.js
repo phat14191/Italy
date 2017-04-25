c3f63872887b88ac7bd5a8666a3f92d183a404fc
halle tempest and phat nguyen <phat@>
2017-04-25 11:55:13 -0700

    Fabulous country Italia

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(function() {
+  $(".clickable").click(function() {
+    $("#is").toggle();
+    $("#ih").toggle();
+  });
+});
+
+$(function() {
+  $(".clickable2").click(function() {
+    $("#as").toggle();
+    $("#ah").toggle();
+  });
+});
+//
